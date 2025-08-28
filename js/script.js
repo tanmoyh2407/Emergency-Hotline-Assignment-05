34b4367aaa30b52e0598df2827eb6e0838a3f85a
Tanmoy <tanmoyh2407@gmail.com>
2025-08-28 14:56:40 +0600

    call history feature complete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Heart Counter function
 
-document.querySelectorAll('.heart_count').forEach(function(heart){
-    heart.addEventListener("click", function(){
+document.querySelectorAll('.heart_count').forEach(function (heart) {
+    heart.addEventListener("click", function () {
         const totalHeart = parseInt(document.getElementById('total_heart').innerText);
         const totalHeartUpdate = totalHeart + 1;
         document.getElementById('total_heart').innerText = totalHeartUpdate;
@@ -11,27 +11,49 @@ document.querySelectorAll('.heart_count').forEach(function(heart){
 // Handle call button function
 
 function handleCallClick(btn) {
-        const totalCoin = parseInt(document.getElementById('total_coin').innerText);
+    const totalCoin = parseInt(document.getElementById('total_coin').innerText);
 
-        if(totalCoin < 20){
-            alert("❌ আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
-            return;
-        }
-            const card = btn.closest('.emergency-card');
-            const serviceNameEng = card.querySelector('.service_name_eng').innerText;
-            const serviceNameBng = card.querySelector('.service_name_bng').innerText;
-            const callNumber = card.querySelector('.call_number').innerText;
+    if (totalCoin < 20) {
+        alert("❌ আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
+        return;
+    }
+    const card = btn.closest('.emergency-card');
+    const serviceNameEng = card.querySelector('.service_name_eng').innerText;
+    const serviceNameBng = card.querySelector('.service_name_bng').innerText;
+    const callNumber = card.querySelector('.call_number').innerText;
 
-            alert(`📞 Calling ${serviceNameEng} ${callNumber}...`);
+    alert(`📞 Calling ${serviceNameEng} ${callNumber}...`);
 
-        const coinAfterPerCall = totalCoin - 20;
-        document.getElementById('total_coin').innerText = coinAfterPerCall;
-        }
+    const coinAfterPerCall = totalCoin - 20;
+    document.getElementById('total_coin').innerText = coinAfterPerCall;
+
+// Call History Add...
+    const historyList = document.getElementById('call_history_list');
+    const timeNow = new Date().toLocaleTimeString();
+
+    const historyItem = document.createElement('div');
+    historyItem.className = "bg-[#FAFAFA] rounded-[8px] p-4 flex justify-between items-center";
+
+    historyItem.innerHTML =
+        `<div>
+            <div class="text-lg text-[#111111]">
+                ${serviceNameBng}
+            </div>
+            <div class="text-[#5C5C5C] text-lg">
+                ${callNumber}
+            </div>
+        </div>
+        <div class="text-lg text-[#111111]">
+            ${timeNow}
+        </div>`;
+
+        historyList.append(historyItem);
+}
 
 // Handle copy button function
 
-document.querySelectorAll('.copy-btn').forEach(function(button) {
-    button.addEventListener("click", function() {
+document.querySelectorAll('.copy-btn').forEach(function (button) {
+    button.addEventListener("click", function () {
         const number = button.closest('.emergency-card').querySelector('.call_number').innerText;
         navigator.clipboard.writeText(number);
         alert(`Copy this number: ${number}`);
